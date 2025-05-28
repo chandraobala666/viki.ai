9543c0250de6979024d09f3a0c1367121b7edc83
gaddam.rahul@gmail.com <gaddam.rahul@gmail.com>
2025-05-28 16:29:03 +0530

    initial Draft

--- ui/components/viki-header/viki-header.js
+++ ui/components/viki-header/viki-header.js
@@ -4,6 +4,16 @@ export class VikiHeader extends BaseComponent {
     constructor() {
         super('viki-header');
     }
+
+    async connectedCallback() {
+        const shadowRoot = await super.connectedCallback();
+        this.componentReady(shadowRoot);
+    }
+    
+    componentReady(shadowRoot) {
+        // Additional initialization can be added here if needed
+        console.log('VIKI Header component is ready');
+    }
 }
 
 customElements.define('viki-header', VikiHeader);
